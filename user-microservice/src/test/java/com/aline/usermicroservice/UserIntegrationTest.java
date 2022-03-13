ac4a200ef207e7aa8d24bf4093805cef8a09406c
David Wright <david.wright@smoothstack.com>
2022-03-12 20:43:48 -0600

    Uncomment tests

--- user-microservice/src/test/java/com/aline/usermicroservice/UserIntegrationTest.java
+++ user-microservice/src/test/java/com/aline/usermicroservice/UserIntegrationTest.java
@@ -322,44 +322,44 @@ class UserIntegrationTest {
     @Nested
     @DisplayName("User Registration Confirmation")
     class UserRegistrationConfirmation {
-        // @Test
-        // void test_registerUser_creates_a_userRegistrationToken() throws Exception {
-        //     // Create user first.
-        //     User user = createDefaultMemberUser("testboy");
+        @Test
+        void test_registerUser_creates_a_userRegistrationToken() throws Exception {
+            // Create user first.
+            User user = createDefaultMemberUser("testboy");
 
-        //     assertNotNull(user);
+            assertNotNull(user);
 
-        //     UserRegistrationToken token = tokenRepository.findByUserId(user.getId()).orElse(null);
+            UserRegistrationToken token = tokenRepository.findByUserId(user.getId()).orElse(null);
 
-        //     assertNotNull(token);
-        // }
+            assertNotNull(token);
+        }
 
-        // @Test
-        // void test_confirmUserRegistration_enables_user_when_token_is_valid_and_user_exists() throws Exception {
-        //     // Create user first.
-        //     User user = createDefaultMemberUser("testboy");
+        @Test
+        void test_confirmUserRegistration_enables_user_when_token_is_valid_and_user_exists() throws Exception {
+            // Create user first.
+            User user = createDefaultMemberUser("testboy");
 
-        //     assertNotNull(user);
+            assertNotNull(user);
 
-        //     UserRegistrationToken token = tokenRepository.findByUserId(user.getId()).orElse(null);
+            UserRegistrationToken token = tokenRepository.findByUserId(user.getId()).orElse(null);
 
-        //     assertNotNull(token);
+            assertNotNull(token);
 
-        //     String tokenString = token.getToken().toString();
+            String tokenString = token.getToken().toString();
 
-        //     ConfirmUserRegistration confirmUserRegistration = ConfirmUserRegistration.builder()
-        //             .token(tokenString)
-        //             .build();
+            ConfirmUserRegistration confirmUserRegistration = ConfirmUserRegistration.builder()
+                    .token(tokenString)
+                    .build();
 
-        //     String body = mapper.writeValueAsString(confirmUserRegistration);
+            String body = mapper.writeValueAsString(confirmUserRegistration);
 
-        //     mockMvc.perform(post("/users/confirmation")
-        //                     .contentType(MediaType.APPLICATION_JSON)
-        //                     .content(body))
-        //             .andExpect(jsonPath("$.username").value(user.getUsername()))
-        //             .andExpect(jsonPath("$.enabled").value(true))
-        //             .andDo(print());
-        // }
+            mockMvc.perform(post("/users/confirmation")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(body))
+                    .andExpect(jsonPath("$.username").value(user.getUsername()))
+                    .andExpect(jsonPath("$.enabled").value(true))
+                    .andDo(print());
+        }
     }
 
     @Nested
